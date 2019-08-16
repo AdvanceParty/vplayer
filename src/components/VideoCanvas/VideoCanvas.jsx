33462f4536d10d4d0ba965c61901f2daf5b01af9
Gil Fewster <hello@gilfewster.com>
2019-08-16 10:26:14 +1000

    Video element created programmatically and not added to DOM (Performance)

--- src/components/VideoCanvas/VideoCanvas.jsx
+++ src/components/VideoCanvas/VideoCanvas.jsx
@@ -1,3 +1,7 @@
+// ToDo:
+// SEE Video/Canvas optimisations at 
+// https://stackoverflow.com/questions/21197707/html5-video-to-canvas-playing-very-slow
+
 import React, { Component } from 'react'
 import styles from './Styles.module.css';
 
@@ -5,55 +9,71 @@ export class VideoCanvas extends Component {
 
   constructor (props) {
     super(props)
+    
+    // ToDo: Why don't refs initialise unless in state?
+    // this.vRef = React.createRef();
+    // this.cRef = React.createRef();
+
     this.state = {
-      vRef: React.createRef(),
       cRef: React.createRef(),
-      video: null,
-      canvas: null,
-      context: null,
-      width: 0,
-      height: 0
-    }
       
+      context: null,
+      videoWidth: 0,
+      videoHeight: 0,
+      ratio: 0
+    }
+    
+    this.video = document.createElement('video');
+    this.video.preload = 'auto';
+    this.video.addEventListener('play', () => this.draw())
+    this.video.addEventListener('canplay', () => this.videoReady());
+    this.video.src = this.props.src;
+
+    this.canvas = null;
+
   }
 
-  componentDidMount = () => {
-    const {vRef, cRef } = this.state;    
-    
-    const video = vRef.current;
-    const canvas = cRef.current;
-    const context = canvas.getContext('2d');
+  getSize = () => ({width:this.canvas.width || 0 , height:this.canvas.height || 0});
+  getRatio = () => (this.state.ratio);
+  getNativeSize = () => ({width:this.state.videoWidth, height:this.state.videoHeight});
 
-    this.setState({video, canvas, context});
+  componentDidMount = () => {    
+    this.canvas = this.state.cRef.current;
+    this.setState({context:this.canvas.getContext('2d')});
+  }
 
-    video.addEventListener('play', () => this.draw())
+  
+  // ToDO: Would be better to do this on resize event
+  setCanvasSize = (w, h) => {
+    const { width, height } = this.canvas;
+    if (w !== width || h !== height) {
+      console.log("Setting Canvas Size")
+      this.canvas.width = w;
+      this.canvas.height = h;
+    }
+  }
+
+  videoReady = () => {
+    const {videoWidth, videoHeight} = this.video
+    this.setState({videoWidth, videoHeight, ratio:videoWidth/videoHeight});
+    this.video.play();
   }
 
   draw = () => {
-    const { video, canvas, context, width, height } = this.state;
-    if(video.paused || video.ended) return false;
-   
-    // ToDO: Would be better to do this on resize event
-    // Rather than checking each time draw() is called
-    const w = video.clientWidth;
-    const h = video.clientHeight;
-    if (w !== width || h !== height) {
-      canvas.width = w;
-      canvas.height = h;
-      this.setState({width: w, height: h})
-    }
-
-    context.drawImage(video,0,0,w,h);
-    setTimeout(this.draw,5);
+    const { context } = this.state;
+    if(this.video.paused || this.video.ended) return false;    
+    
+    const {width, height} = this.getNativeSize()
+    this.setCanvasSize(width,height);
+    context.drawImage(this.video,0,0,width,height);
+    
+    setTimeout(this.draw,20);
   }
 
   render() {
     return (
       <div>
         <canvas ref={this.state.cRef}></canvas>
-        <video ref={this.state.vRef} autoPlay >
-          <source src={this.props.src} type={this.props.type} />
-        </video>
       </div>
     )
   }
